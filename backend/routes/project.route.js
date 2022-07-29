2805392e47abd84c10be5ca1347fb54661350f14
Alejandro Morales <alejandro.morales.a@gmail.com>
2022-07-28 22:29:50 -0400

    calculate proiject cost and order by cost

--- backend/routes/project.route.js
+++ backend/routes/project.route.js
@@ -18,31 +18,52 @@ console.log(req.query.user)
  }
   
 });
-// not ready yet im working on it
-// router.route('/calculateCost').get((req, res) => {
-//   //if there is param user with the user id returns the projects where the user is assigned
-//   let projectId=req.query.projectId;
-//    if(projectId!=undefined && projectId!=''){
-//     Task.find({project: projectId}).
-//     then(tasks=>{
-//       let cost=0;
-//       let hours=0;
-//       tasks.forEach(task=>{
-//         if(task.endDate!=undefined){
-//           User.findById(task.assignee).
-//         }
-//       })
-//     }).
-//     catch();
 
-//     Project.find({users: req.query.user})
-//     .then(projects => res.json(projects))
-//     .catch(err => res.status(400).json('Error: ' + err));
-//    }else{
-//     res.status(400).json('Error: ' + "Send a valid project id")
-//    }
+router.route('/calculateCost').get((req, res) => {
+
+  let projectId=req.query.projectId;
+   if(projectId!=undefined && projectId!=''){
+    calculateCost(projectId).then(result=>{
+    console.log('result sent'+result)
+    res.json(result)}).
+    catch(err => res.status(400).json('Error: ' + err));
     
-//   });
+    
+   }else{
+    res.status(400).json('Error: ' + "Send a valid project id")
+   }
+    
+  });
+  router.route('/sortByCost').get((req, res) => {
+    let OrderedList=[]
+    Project.find()
+    .then(projects => {
+      let idx=projects.length;
+      console.log("lenght "+idx)
+    if(idx>0){
+      projects.forEach(project=>{
+        console.log("Called here")
+        calculateCost(project.id).then(result=>{
+          console.log(project.id+" :::: "+result)
+          console.log(idx)
+          idx--;
+          OrderedList.push({"project":project,"cost":result.total})
+          if(idx==0){
+            console.log(OrderedList)
+            res.json(OrderedList.sort(comparatorDown))
+          }
+        }).
+          catch(err => res.status(400).json('Error: ' + err));
+          
+      })
+    }
+      // res.json(projects)
+    })
+    .catch(err => res.status(400).json('Error: ' + err));
+    
+    
+      
+    });
 
 router.route('/add').post((req, res) => {
   const project = req.body;
@@ -63,5 +84,86 @@ router.route('/edit').post((req, res) => {
    .catch(err => res.status(400).json('Error: ' + err));
 
 });
+
+
+ async function calculateCost(projectId){
+  console.log("called")
+  return new Promise(result =>{
+    Task.find({project: projectId}).
+  then(tasks=>{
+    
+    let cost=0;
+    let hours=0;
+    let idx=tasks.length;
+    if(idx>0){
+    tasks.forEach(task=>{
+      let hourlyRate;
+      let taskHours;
+      if(task.endDate!=undefined){
+        User.findById(task.assignee).then(
+          user=>{
+            console.log(user)
+            hourlyRate=user.hourlyRate
+            taskHours=Math.abs(task.endDate - task.startDate) / 36e5;
+            hours+=taskHours;
+            cost+=(hourlyRate*taskHours)
+            idx--;
+            if(idx==0){
+            let final={"total":cost,"hours":hours}
+            console.log("answered")
+            result(final)
+            }
+          }
+        ).catch(err=>{
+          console.log("answered err")
+    throw(err)
+        })
+      }else{
+        idx--;
+        if(idx==0){
+          let final={"total":cost,"hours":hours}
+          console.log("answered")
+          result(final)
+          }
+      }
+      
+    })
+  }else{
+    let final={"total":cost,"hours":hours}
+    console.log("answered")
+    result(final)
+  }
+    
+  }).
+  catch(err => {
+    console.log("answered err")
+    throw(err)});
+
+  }
+
+  );
+  
+}
+
+function comparatorUp(a, b) {
+
+  let comparison = 0;
+  if (a.cost > b.cost) {
+      comparison = 1;
+  } else if (a.cost < b.cost) {
+      comparison = -1;
+  }
+  return comparison;
+}
+function comparatorDown(a, b) {
+
+  let comparison = 0;
+  if (a.cost > b.cost) {
+      comparison = -1;
+  } else if (a.cost < b.cost) {
+      comparison = 1;
+  }
+  return comparison;
+}
 
 module.exports = router;
